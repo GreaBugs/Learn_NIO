c7ed2b0618b8da1a166b3134100b6f08f2b9047b
baoyue.shen <shenbaoyue@126.com>
2024-09-10 16:09:20 +0800

    通过分布式锁完善用户注册功能，防止恶意请求短时间内用同一未注册用户名注册新用户

---
+++ admin/src/main/java/com/getoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.getoffer.shortlink.admin.common.constant;
+
+/**
+ * 短链接后端管理 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+}
--- admin/src/main/java/com/getoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/getoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -14,9 +14,12 @@ import com.getoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static com.getoffer.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static com.getoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
 import static com.getoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
@@ -28,7 +31,11 @@ import static com.getoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_S
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
+    // 注入布隆过滤器
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+
+    // 分布式锁
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username){
@@ -47,7 +54,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public boolean hasUsername(String username){
-        return !userRegisterCachePenetrationBloomFilter.contains(username);
+        return userRegisterCachePenetrationBloomFilter.contains(username);
     }
 
     /**
@@ -61,16 +68,26 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
             log.info("用户名已经存在");
             throw new ClientException(USER_NAME_EXIST);
         }
-        // 2. 如果用户名不存在，插入新的用户数据
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        // 3. 检查插入结果，如果插入失败，抛出异常
-        if (inserted < 1){
-            log.info("用户名插入失败");
-            throw new ClientException(USER_SAVE_ERROR);
+        // 分布式锁       作为锁的前缀，加上用户名,确保每个用户的注册操作有一个独立的锁
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try{
+            if (lock.tryLock()){  // 尝试获取锁。如果当前没有其他线程在处理相同用户名的注册请求，锁会成功获取。如果锁无法获取，意味着另一个线程已经在处理相同的用户名注册，此时会抛出异常。
+                // 2. 用户名不存在，插入新的用户数据
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                // 3. 检查插入结果，如果插入失败，抛出异常
+                if (inserted < 1){
+                    log.info("用户名插入失败");
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                log.info("成功插入用户名" + requestParam.getUsername());
+                // 4. 使用布隆过滤器防止缓存穿透，新增已注册的用户名
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        }finally {
+            lock.unlock();  // 保证了即使在代码发生异常的情况下，也会释放分布式锁，防止锁的永久持有。
         }
-        log.info("成功插入用户名" + requestParam.getUsername() + "已经存在");
-        // 4. 使用布隆过滤器防止缓存穿透，新增已注册的用户名
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 
 }
