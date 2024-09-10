62892a4804ee75584cd1abbf9c69dbf226bd12c3
baoyue.shen <shenbaoyue@126.com>
2024-09-10 20:46:12 +0800

    用户表通过ShardingSphere完成分表处理

---
+++ admin/src/test/java/com/getoffer/shortlink/admin/test/TestShardingSphere.java
@@ -0,0 +1,26 @@
+package com.getoffer.shortlink.admin.test;
+
+public class TestShardingSphere {
+
+    public static final String SQL_TEMPLATE = "CREATE TABLE t_user_%d ( \n" +
+            "  id BIGINT AUTO_INCREMENT COMMENT 'ID', \n" +
+            "  username VARCHAR(25) CHARACTER SET utf8mb4 NULL COMMENT '用户名', \n" +
+            "  password VARCHAR(51) CHARACTER SET utf8mb4 NULL COMMENT '密码', \n" +
+            "  real_name VARCHAR(25) CHARACTER SET utf8mb4 NULL COMMENT '真实姓名', \n" +
+            "  phone VARCHAR(12) CHARACTER SET utf8mb4 NULL COMMENT '手机号', \n" +
+            "  mail VARCHAR(51) CHARACTER SET utf8mb4 NULL COMMENT '邮件', \n" +
+            "  deletion_time BIGINT NULL COMMENT '注销时间戳', \n" +
+            "  create_time DATETIME NULL COMMENT '创建时间', \n" +
+            "  update_time DATETIME NULL COMMENT '修改时间', \n" +
+            "  del_flag TINYINT NULL COMMENT '删除标识 0: 未删除 1: 已删除', \n" +  // 设置 del_flag 默认值为 0
+            "  PRIMARY KEY (id), \n" +
+            "  UNIQUE KEY idx_unique_username (username) \n" +  // 省略 USING BTREE，因为 BTREE 是默认的
+            ") COMMENT '用户表';";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            String sql = String.format(SQL_TEMPLATE, i);
+            System.out.println(sql);  // 输出生成的 SQL 语句到控制台，确保格式正确
+        }
+    }
+}
